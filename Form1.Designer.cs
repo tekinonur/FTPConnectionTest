88eb3287999241489a9040390eda67fc30531f91
Onur Tekin <onurtekin314@gmail.com>
2021-06-28 11:13:08 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace FTPConnectionTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_Host = new System.Windows.Forms.TextBox();
+            this.textBox_Port = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_Username = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_Password = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button_Test = new System.Windows.Forms.Button();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 29);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Host:";
+            // 
+            // textBox_Host
+            // 
+            this.textBox_Host.Location = new System.Drawing.Point(119, 26);
+            this.textBox_Host.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox_Host.Name = "textBox_Host";
+            this.textBox_Host.Size = new System.Drawing.Size(257, 24);
+            this.textBox_Host.TabIndex = 1;
+            this.textBox_Host.Text = "test.rebex.net";
+            // 
+            // textBox_Port
+            // 
+            this.textBox_Port.Location = new System.Drawing.Point(119, 62);
+            this.textBox_Port.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_Port.Name = "textBox_Port";
+            this.textBox_Port.Size = new System.Drawing.Size(257, 24);
+            this.textBox_Port.TabIndex = 3;
+            this.textBox_Port.Text = "22";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 65);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port:";
+            // 
+            // textBox_Username
+            // 
+            this.textBox_Username.Location = new System.Drawing.Point(119, 95);
+            this.textBox_Username.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_Username.Name = "textBox_Username";
+            this.textBox_Username.Size = new System.Drawing.Size(257, 24);
+            this.textBox_Username.TabIndex = 5;
+            this.textBox_Username.Text = "demo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 98);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Username:";
+            // 
+            // textBox_Password
+            // 
+            this.textBox_Password.Location = new System.Drawing.Point(119, 129);
+            this.textBox_Password.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_Password.Name = "textBox_Password";
+            this.textBox_Password.Size = new System.Drawing.Size(257, 24);
+            this.textBox_Password.TabIndex = 7;
+            this.textBox_Password.Text = "password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 132);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Password:";
+            // 
+            // button_Test
+            // 
+            this.button_Test.Location = new System.Drawing.Point(35, 161);
+            this.button_Test.Name = "button_Test";
+            this.button_Test.Size = new System.Drawing.Size(341, 39);
+            this.button_Test.TabIndex = 8;
+            this.button_Test.Text = "TEST";
+            this.button_Test.UseVisualStyleBackColor = true;
+            this.button_Test.Click += new System.EventHandler(this.button_Test_Click);
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Location = new System.Drawing.Point(35, 206);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.Size = new System.Drawing.Size(341, 110);
+            this.textBoxLog.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 339);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.button_Test);
+            this.Controls.Add(this.textBox_Password);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox_Username);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_Port);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_Host);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "FTP Connection Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_Host;
+        private System.Windows.Forms.TextBox textBox_Port;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_Username;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_Password;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button_Test;
+        private System.Windows.Forms.TextBox textBoxLog;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FTPConnectionTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Test_Click(object sender, EventArgs e)
+        {
+            textBoxLog.Clear();
+            string host = textBox_Host.Text;
+            int port = 0;
+            if(!Int32.TryParse(textBox_Port.Text,out port))
+            {
+                MessageBox.Show("Port must be an integer!");
+                return;
+            }
+            string username = textBox_Username.Text;
+            string password = textBox_Password.Text;
+
+            try
+            {
+                using (var client = new SftpClient(host, port, username, password))
+                {
+                    client.Connect();
+                    textBoxLog.AppendText("Connected to "+host+Environment.NewLine);
+                    textBoxLog.AppendText("Connection status "+client.IsConnected+Environment.NewLine);
+                }
+            }
+            catch (Exception ex)
+            {
+                textBoxLog.AppendText(ex.Message);
+                MessageBox.Show("Connection error:" + ex.Message);
+            }
+        }
+    }
+}
